51351b3bb955a243002fc24beadc53a1781d7538
LokanandaHJ <97349849+LokanandaHJ@users.noreply.github.com>
2022-03-24 14:04:37 +0530

    update

--- src/Redux/store.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import cartReducer from "./Reducers/cart.reducers";
-import { createStore } from "redux";
-
-const store = createStore(cartReducer);
-export default store;
